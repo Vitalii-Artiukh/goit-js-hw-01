c5adf585da784d2c74fb344bbdfa5b01b4c61e77
Vitalii Artiukh <radial_car@yahoo.com>
2024-06-08 20:07:06 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -16,27 +16,3 @@ function getElementWidth(content, padding, border) {
 console.log(getElementWidth("50px", "8px", "4px"));
 console.log(getElementWidth("60px", "12px", "8.5px"));
 console.log(getElementWidth("200px", "0px", "0px"));
-
-
-
-// console.log(
-//   getElementWidth(
-//     Number.parseFloat("50px"),
-//     Number.parseFloat("8px"),
-//     Number.parseFloat("4px")
-//   )
-// );
-// console.log(
-//   getElementWidth(
-//     Number.parseFloat("60px"),
-//     Number.parseFloat("12px"),
-//     Number.parseFloat("8.5px")
-//   )
-// );
-// console.log(
-//   getElementWidth(
-//     Number.parseFloat("200px"),
-//     Number.parseFloat("0px"),
-//     Number.parseFloat("0px")
-//   )
-// );
